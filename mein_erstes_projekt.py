66895df3582315004a91a304862048fc29b98082
Simona-Girjoaba <newgirjoabasimonamaria@gmail.com>
2026-03-06 13:12:35 +0100

    Mein erstes Python Projekt - Simona

---
+++ mein_erstes_projekt.py
@@ -0,0 +1,18 @@
+#Mein erstes Python Projekt
+#Simona Girjoaba AI & Data Science Journey
+#Datum: März 2026
+
+#Persönliche Vorstellung
+name ="Simona Girjoaba"
+beruf="Ai Security Engineer in Ausbildung"
+ziele=["Data Science","AI security","Nachhaltigkeit"]
+
+print("="*50)
+print("Hello, ich bin", name)
+print("Mein Ziel:",beruf)
+print("="*50)
+
+print("\n Meine Lernziele:")
+for ziel in ziele:
+    print("->",ziel)
+print("\nPython funktioniert perfekt!🚀")
